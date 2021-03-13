04ee563e77fed4bbbd90857f9f0ed66feb9f732b
Mike Watkins <6672701+PurpleHominid@users.noreply.github.com>
2021-03-13 20:24:35 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char ** argv){
+    
+    printf("\n");
+    printf("\n\x1B[31mRED\033[0m\t\t");
+    printf("\n\x1B[32mGREEN\033[0m\t\t");
+    printf("\n\x1B[33mOrange\033[0m\t\t");
+    printf("\n\x1B[34mBlue\033[0m\t\t");
+    printf("\n\x1B[35mPurple\033[0m\n");
+    
+    printf("\n\x1B[36mLightblue\033[0m\t\t");
+    printf("\n\x1B[36mLightblue\033[0m\t\t");
+    printf("\n\x1B[36mLightblue\033[0m\t\t");
+    printf("\n\x1B[37mWhite\033[0m\t\t");
+    printf("\n\x1B[93mYellow\033[0m\n");
+    
+    printf("\n\033[3;42;30mGreen back\033[0m\t\t");
+    printf("\n\033[3;43;30mOrange back\033[0m\t\t");
+    printf("\n\033[3;44;30mBlue black\033[0m\t\t");
+    printf("\n\033[3;104;30mLight blue back\033[0m\t\t");
+    printf("\n\033[3;100;30mGray Back\033[0m\n");
+
+    printf("\n\033[3;47;35mTexting\033[0m\t\t");
+    printf("\n\033[2;47;35mTexting\033[0m\t\t");
+    printf("\n\033[1;47;35mTexting\033[0m\t\t");
+    printf("\n\t\t");
+    printf("\n\n");
+
+    return 0;
+}
